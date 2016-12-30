95f8bb41412121489309e05b572ce91ae6eed6f5
Jaspreet Singh <jaspreet_xps@yahoo.com>
2016-12-29 17:32:39 -0800

    Update promise to be the same regardless of deployment

--- src/client/actions/index.js
+++ src/client/actions/index.js
@@ -2,13 +2,7 @@ import axios from 'axios'
 import { GET_BLOGPOST_LINKS, GET_BLOGPOST_THUMBNAILS } from './types'
 
 export function getBlogpostLinks() {
-  let promise;
-  if (window.location.hostname.indexOf("localhost") > -1) {
-    promise = axios.get(`/api/blogposts`)
-  }
-  else {
-    promise = axios.get(`http://www.jaspreetsingh.me:5050/api/blogposts`)
-  }
+  const promise = axios.get(`/api/blogposts`)
   return {
     type: GET_BLOGPOST_LINKS,
     payload: promise
@@ -16,13 +10,7 @@ export function getBlogpostLinks() {
 }
 
 export function getBlogpostThumbnails() {
-  let promise;
-  if (window.location.hostname.indexOf("localhost") > -1) {
-    promise = axios.get(`/api/thumbnails`)
-  }
-  else {
-    promise = axios.get(`http://www.jaspreetsingh.me:5050/api/thumbnails`)
-  }
+  const promise = axios.get(`/api/thumbnails`)
   return {
     type: GET_BLOGPOST_THUMBNAILS,
     payload: promise
