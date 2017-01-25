cf63b3291ec8beef8ed3a7ec06a4df55ab988ee4
Jaspreet Singh <jaspreet_xps@yahoo.com>
2017-01-24 17:53:56 -0800

    Work in progress to port graphql over

--- src/server/index.js
+++ src/server/index.js
@@ -1,20 +1,44 @@
 import express from 'express'
 import path from 'path'
 import cors from 'cors'
+import pg from 'pg'
+import DataLoader from 'dataloader'
+import { graphql } from 'graphql'
+import graphqlHTTP from 'express-graphql'
+import pgdbCreator from '../../database/pgdb'
+import pgConfigs from '../../postgres/config'
 import compression from 'compression'
 import getBlogpostLinks from './requestHandlers/getBlogPostLinks'
 import getBlogThumbnails from './requestHandlers/getBlogThumbnails'
 
 // express app
 const app = express()
+const nodeEnv = process.env.NODE_ENV
 
-const isProd = process.env.NODE_ENV === 'production'
+// PG setup
+const pgConfig = pgConfigs[nodeEnv]
+const pgPool = new pg.Pool(pgConfig)
+const pgdb = pgdbCreator(pgPool)
+
+const isProd = nodeEnv === 'production'
 
 // compression
 app.use(compression())
 
 // cors
 app.use(cors())
+
+app.use('/graphql', (req, res) => {
+  const loaders = {}
+  graphqlHTTP({
+    schema: rootSchema,
+    graphiql: true,
+    context: {
+      pgPool,
+      loaders
+    }
+  })(req, res)
+})
 
 // static
 const maxAge = isProd ? 1000 * 60 * 60 * 24 * 30 : 0
